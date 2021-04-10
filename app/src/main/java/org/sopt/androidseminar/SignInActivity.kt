5b1b4dfa1f37d275a81395e2d3e54c2f2928c20b
lydiacho <sheecho0804@ewhain.net>
2021-04-11 00:04:14 +0900

    세미나_과제1

--- app/src/main/java/org/sopt/androidseminar/SignInActivity.kt
+++ app/src/main/java/org/sopt/androidseminar/SignInActivity.kt
@@ -4,15 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import org.sopt.androidseminar.databinding.ActivityMainBinding
+import androidx.activity.result.contract.ActivityResultContracts
+import org.sopt.androidseminar.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivitySignInBinding
 
+    private val signUpActivityLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+    ){
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         initButtonClickEvent()
@@ -20,21 +26,22 @@ class SignInActivity : AppCompatActivity() {
 
     private fun initButtonClickEvent() {
         binding.loginButton.setOnClickListener {
-            val userID = binding.editID.text
-            val userPW = binding.editPW.text
+            val intent = Intent(this@SignInActivity, HomeActivity::class.java)
+            val userID = binding.editID.text.toString()
+            val userPW = binding.editPW.text.toString()  //toString이 뭔지 좀더 알아보기
             if (userID.isNullOrBlank() || userPW.isNullOrBlank()) {
                 Toast.makeText(this@SignInActivity, "아이디/비밀번호를 확인해주세요!", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(this@SignInActivity, "로그인성공", Toast.LENGTH_SHORT).show()
                 val toHomeIntent = Intent(this@SignInActivity, HomeActivity::class.java)
                 toHomeIntent.putExtra("userID",userID)
-                startActivity(toHomeIntent)//HomeActivity로 이동해지지가 않음......
+                startActivity(toHomeIntent)
             }
         }
 
         binding.signButton.setOnClickListener {
             val toSignIntent = Intent(this@SignInActivity, SignupActivity::class.java)
-            startActivity(toSignIntent)  //registerActivityForResult 어떻게..
+            signUpActivityLauncher.launch(toSignIntent)
         }
 
     }
--- app/src/main/java/org/sopt/androidseminar/SignupActivity.kt
+++ app/src/main/java/org/sopt/androidseminar/SignupActivity.kt
@@ -1,10 +1,11 @@
 package org.sopt.androidseminar
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import org.sopt.androidseminar.databinding.ActivityMainBinding
+import org.sopt.androidseminar.databinding.ActivitySignInBinding
 import org.sopt.androidseminar.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
@@ -21,17 +22,17 @@ class SignupActivity : AppCompatActivity() {
 
     private fun initButtonClickEvent() {
         binding.signButton.setOnClickListener {
-            val toHomeIntent2 = Intent(this@SignupActivity, HomeActivity::class.java)  //초기 signupActivity로 돌아간다? 종료?
-            val userName = binding.editName.text
-            val userID = binding.editGithubID.text
-            val userPW = binding.editPW.text
+            val toSignInIntent = Intent(this@SignupActivity, SignInActivity::class.java)
+            val userName = binding.editName.text.toString()
+            val userID = binding.editGithubID.text.toString()
+            val userPW = binding.editPW.text.toString()
             if (userName.isNullOrBlank() || userID.isNullOrBlank() || userPW.isNullOrBlank()) {
                 Toast.makeText(this@SignupActivity, "빈 칸이 있는지 확인해주세요", Toast.LENGTH_SHORT).show()
             } else {
-                toHomeIntent2.putExtra("userName", userName)
-                toHomeIntent2.putExtra("userID", userID)
-                startActivity(toHomeIntent2)
-                //putExtra로 모든 값을 intent에 넣어 전달, SingUpActivity로 돌아가도록 종료
+                toSignInIntent.putExtra("userID", userID)
+                toSignInIntent.putExtra("userPW", userPW)
+                setResult(Activity.RESULT_OK, toSignInIntent)
+                finish()
             }
         }
     }
