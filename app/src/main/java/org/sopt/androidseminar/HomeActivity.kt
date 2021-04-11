e36409bf2e0d89733df863907a09b2f56f44b52e
lydiacho <sheecho0804@ewhain.net>
2021-04-11 13:06:22 +0900

    세미_과제1

--- app/src/main/java/org/sopt/androidseminar/HomeActivity.kt
+++ app/src/main/java/org/sopt/androidseminar/HomeActivity.kt
@@ -3,6 +3,7 @@ package org.sopt.androidseminar
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import org.sopt.androidseminar.databinding.ActivityHomeBinding
 import org.sopt.androidseminar.databinding.ActivitySignupBinding
 
@@ -12,8 +13,33 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("HomeLog", "onCreate")
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d("HomeLog", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("HomeLog", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("HomeLog", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("HomeLog", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("HomeLog", "onDestroy")
+    }
 }
--- app/src/main/java/org/sopt/androidseminar/SignInActivity.kt
+++ app/src/main/java/org/sopt/androidseminar/SignInActivity.kt
@@ -3,13 +3,15 @@ package org.sopt.androidseminar
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import android.widget.Toast.makeText
 import androidx.activity.result.contract.ActivityResultContracts
-import org.sopt.androidseminar.databinding.ActivitySignInBinding
+import org.sopt.androidseminar.databinding.ActivityMainBinding
 
 class SignInActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySignInBinding
+    private lateinit var binding: ActivityMainBinding
 
     private val signUpActivityLauncher = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
@@ -18,7 +20,8 @@ class SignInActivity : AppCompatActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignInBinding.inflate(layoutInflater)
+        Log.d("SignInLog", "onCreate")
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         initButtonClickEvent()
@@ -26,13 +29,14 @@ class SignInActivity : AppCompatActivity() {
 
     private fun initButtonClickEvent() {
         binding.loginButton.setOnClickListener {
-            val intent = Intent(this@SignInActivity, HomeActivity::class.java)
             val userID = binding.editID.text.toString()
             val userPW = binding.editPW.text.toString()  //toString이 뭔지 좀더 알아보기
             if (userID.isNullOrBlank() || userPW.isNullOrBlank()) {
-                Toast.makeText(this@SignInActivity, "아이디/비밀번호를 확인해주세요!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@SignInActivity, "아이디/비밀번호를 확인해주세요!", Toast.LENGTH_SHORT)
+                        .show()
             } else {
-                Toast.makeText(this@SignInActivity, "로그인성공", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@SignInActivity, "로그인성공", Toast.LENGTH_SHORT)
+                        .show()
                 val toHomeIntent = Intent(this@SignInActivity, HomeActivity::class.java)
                 toHomeIntent.putExtra("userID",userID)
                 startActivity(toHomeIntent)
@@ -44,5 +48,30 @@ class SignInActivity : AppCompatActivity() {
             signUpActivityLauncher.launch(toSignIntent)
         }
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("SignInLog", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("SignInLog", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("SignInLog", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("SignInLog", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("SignInLog", "onDestroy")
     }
 }
